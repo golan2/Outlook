1a8372ac68766800b11b9f8fe582fde16d49c1f8
Izik Golan <izik.golan@att.com>
2018-04-29 15:32:13 +0300

    change e-mail to hotmail

--- src/Connect2Exchange.java
+++ src/Connect2Exchange.java
@@ -4,8 +4,8 @@ import microsoft.exchange.webservices.data.*;
 
 /**
  * <pre>
- * <B>Copyright:</B>   HP Software IL
- * <B>Owner:</B>       <a href="mailto:izik.golan@hp.com">Izik Golan</a>
+ * <B>Copyright:</B>   Izik Golan
+ * <B>Owner:</B>       <a href="mailto:golan2@hotmail.com">Izik Golan</a>
  * <B>Creation:</B>    15/08/14 11:09
  * <B>Since:</B>       BSM 9.21
  * <B>Description:</B>
